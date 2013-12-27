cb2ecfe8adb4c195795e277214ca4405d0bd0780
runphp <runphp@qq.com>
2013-12-27 13:55:29 +0800

    move "Module.php" into "src/UcenterClient"

---
+++ src/UcenterClient/Module.php
@@ -0,0 +1,55 @@
+<?php
+namespace UcenterClient;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+
+class Module implements AutoloaderProviderInterface
+{
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/../../autoload_classmap.php'
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__
+                )
+            )
+        );
+    }
+
+    public function getConfig($env = null)
+    {
+        return include __DIR__ . '/../../config/module.config.php';
+    }
+
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+                'ucenter_client_module_options' => function ($sm)
+                {
+                    $config = $sm->get('Config');
+                    return new Options\ModuleOptions(isset($config['ucenter']) ? $config['ucenter'] : array());
+                },
+                'ucenter_service' => function ($sm)
+                {
+                    $service = new Services\Ucenter();
+                    $service->setOptions($sm->get('ucenter_client_module_options'));
+                    return $service;
+                }
+            )
+        );
+    }
+
+    public function getControllerPluginConfig()
+    {
+        return array(
+            'invokables' => array(
+                'ucNote' => 'UcenterClient\Controller\Plugin\UcNote'
+            )
+        );
+    }
+}
